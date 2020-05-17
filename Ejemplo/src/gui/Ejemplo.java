a555f211a263441f09c6e0c59341f9e4046a4345
sebas0104 <sebastian.am345@gmail.com>
2020-05-17 00:39:47 -0500

    agregando archivos java

---
+++ Ejemplo/src/gui/Ejemplo.java
@@ -0,0 +1,95 @@
+package gui;
+
+import semana_02.Alumno;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Font;
+
+public class Ejemplo extends JFrame implements ActionListener {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private JPanel contentPane;
+	private JButton btnProcesar;
+	private JScrollPane scrollPane;
+	private JTextArea txtS;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Ejemplo frame = new Ejemplo();
+					frame.setVisible(true);
+				}
+				catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Ejemplo() {
+		setTitle("Ejemplo - Semana_02");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 500);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		btnProcesar = new JButton("Procesar");
+		btnProcesar.addActionListener(this);
+		btnProcesar.setBounds(173, 11, 89, 23);
+		contentPane.add(btnProcesar);
+		
+		scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 48, 414, 403);
+		contentPane.add(scrollPane);
+		
+		txtS = new JTextArea();
+		txtS.setFont(new Font("Monospaced", Font.PLAIN, 13));
+		scrollPane.setViewportView(txtS);
+	}
+	public void actionPerformed(ActionEvent arg0) {
+		if (arg0.getSource() == btnProcesar) {
+			actionPerformedBtnProcesar(arg0);
+		}
+	}
+	protected void actionPerformedBtnProcesar(ActionEvent arg0) {
+		Alumno a = new Alumno(12345, "Juan", 13, 15);
+		listado(a);
+		
+		a.setNota1(19);
+		a.setNota2(18);
+		listado(a);
+	}
+	//  Metodos tipo void (con parametros)	
+	void imprimir(String s) {
+		txtS.append(s + "\n");
+	}
+	void listado(Alumno x) {
+		imprimir("DirMem   :  " + x);
+		imprimir("c�digo   :  " + x.getCodigo());
+		imprimir("nombre   :  " + x.getNombre());
+		imprimir("nota 1   :  " + x.getNota1());
+		imprimir("nota 2   :  " + x.getNota2());
+		imprimir("promedio :  " + x.promedio());
+		imprimir("");
+	}
+	
+}
---
+++ Ejemplo/src/semana_02/Alumno.java
@@ -0,0 +1,45 @@
+package semana_02;
+
+public class Alumno {
+	
+	//  Atributos privados
+	private int codigo, nota1, nota2;
+	private String nombre;
+	//  Constructor
+	public Alumno(int cod, String nom, int n1, int n2) {
+		codigo = cod;
+		nombre = nom;
+		nota1 = n1;
+		nota2 = n2;
+	}
+	//  Metodos de acceso publico: set/get
+	public void setCodigo(int cod) {
+		codigo = cod;
+	}
+	public void setNombre(String nom) {
+		nombre = nom;
+	}
+	public void setNota1(int n1) {
+		nota1 = n1;
+	}
+	public void setNota2(int n2) {
+		nota2 = n2;
+	}
+	public int getCodigo() {
+		return codigo;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public int getNota1() {
+		return nota1;
+	}
+	public int getNota2() {
+		return nota2;
+	}	    		
+	//  Operaciones p�blicas
+	public double promedio() {
+		return (nota1 + nota2) / 2.0;
+	}
+	
+}
